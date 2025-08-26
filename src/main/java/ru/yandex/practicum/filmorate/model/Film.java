f74395b77da28ff4c3d846e4b21d6a93c29269b2
Ilia Egorov <aleilmessia@mail.ru>
2025-08-27 00:39:26 +0300

    fix: changed duration to Duration instead of long

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,13 +1,15 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Month;
 
@@ -21,9 +23,17 @@ public class Film {
     @NotBlank(message = "Description should not be null or empty")
     String description;
     LocalDate releaseDate;
-    @Positive
-    long duration;
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT, pattern = "MINUTES")
+    Duration duration;
 
+    @JsonIgnore
+    @AssertTrue(message = "Film duration must be positive")
+    public boolean isDurationPositive() {
+        if (duration == null) return false;
+        return duration.isPositive();
+    }
+
+    @JsonIgnore
     @AssertTrue(message = "Release date must be after December 28, 1895")
     public boolean isFilmReleaseDateAfter1895() {
         if (releaseDate == null) return true;
