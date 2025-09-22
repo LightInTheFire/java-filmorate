c2bf8b3f92c68bedee93c5eb411f2fa25e425718
Ilia Egorov <aleilmessia@mail.ru>
2025-09-22 15:34:35 +0300

    feat: added init sql script to add genres and mpa ratings in tables.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+MERGE INTO mpa_ratings (name) KEY (name) VALUES ('g');
+MERGE INTO mpa_ratings (name) KEY (name) VALUES ('pg');
+MERGE INTO mpa_ratings (name) KEY (name) VALUES ('pg-13');
+MERGE INTO mpa_ratings (name) KEY (name) VALUES ('r');
+MERGE INTO mpa_ratings (name) KEY (name) VALUES ('nc-17');
+
+MERGE INTO genres (name) KEY (name) VALUES ('комедия');
+MERGE INTO genres (name) KEY (name) VALUES ('драма');
+MERGE INTO genres (name) KEY (name) VALUES ('мультфильм');
+MERGE INTO genres (name) KEY (name) VALUES ('триллер');
+MERGE INTO genres (name) KEY (name) VALUES ('документальный');
+MERGE INTO genres (name) KEY (name) VALUES ('Боевик');
