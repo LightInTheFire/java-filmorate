062cc8721984c397ac8051827998d3e922f055c4
Ilia Egorov <aleilmessia@mail.ru>
2025-09-22 15:34:03 +0300

    feat: updated errorhandler to properly work with ConstraintViolationException and MethodArgumentNotValidException. Added custom annotation to check is date after specified date

--- src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionApiHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionApiHandler.java
@@ -3,11 +3,17 @@ package ru.yandex.practicum.filmorate.handler;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.dto.ErrorResponse;
+import ru.yandex.practicum.filmorate.dto.error.ErrorResponse;
+import ru.yandex.practicum.filmorate.dto.error.ValidationErrorResponse;
+import ru.yandex.practicum.filmorate.dto.error.Violation;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -17,14 +23,35 @@ public class ExceptionApiHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(Exception ex) {
         log.error("Error occurred while processing request {}", ex.getMessage());
-        return new ErrorResponse("internal server error", "An error occurred while processing request");
+        return new ErrorResponse("internal server error",
+                "An error occurred while processing request");
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleValidationErrors(ConstraintViolationException ex) {
-        log.warn("Validation error occurred while processing request {}", ex.getMessage());
-        return new ErrorResponse("validation errors", ex.getMessage());
+    public ValidationErrorResponse onConstraintValidationException(
+            ConstraintViolationException e
+    ) {
+        final List<Violation> violations = e.getConstraintViolations().stream()
+                .map(
+                        violation -> new Violation(
+                                violation.getPropertyPath().toString(),
+                                violation.getMessage()
+                        )
+                )
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(violations);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse onMethodArgumentNotValidException(
+            MethodArgumentNotValidException e
+    ) {
+        final List<Violation> violations = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(violations);
     }
 
     @ExceptionHandler
