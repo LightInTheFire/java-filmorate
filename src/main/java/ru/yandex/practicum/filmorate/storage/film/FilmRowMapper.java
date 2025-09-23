4edbbe68df673d38dfde6ad88a2f528beeb94385
Ilia Egorov <aleilmessia@mail.ru>
2025-09-23 20:01:26 +0300

    feat: added rowmappers for user and film entities

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmRowMapper.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.MPARating;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+@Component
+public class FilmRowMapper implements RowMapper<Film> {
+    @Override
+    public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return Film.builder()
+                .id(rs.getLong("film_id"))
+                .name(rs.getString("name"))
+                .description(rs.getString("description"))
+                .releaseDate(rs.getObject("release_date", LocalDate.class))
+                .duration(rs.getInt("duration_in_minutes"))
+                .mpaRating(new MPARating(rs.getLong("mpa_id")))
+                .build();
+    }
+}
