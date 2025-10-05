a625a92f710ed8b814cf7e53f198a7444994bd6b
Ilia Egorov <aleilmessia@mail.ru>
2025-10-05 12:32:07 +0300

    feat: changed service impls to their interfaces

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -1,31 +1,11 @@
 package ru.yandex.practicum.filmorate.service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dto.genre.GenreDto;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.mapper.GenreMapper;
-import ru.yandex.practicum.filmorate.repository.genre.GenreRepository;
 
 import java.util.Collection;
 
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class GenreService {
-    private final GenreRepository genreRepository;
+public interface GenreService {
+    Collection<GenreDto> findAll();
 
-    public Collection<GenreDto> findAll() {
-        return genreRepository.findAll()
-                .stream()
-                .map(GenreMapper::toGenreDto)
-                .toList();
-    }
-
-    public GenreDto findById(long id) {
-        return genreRepository.findById(id)
-                .map(GenreMapper::toGenreDto)
-                .orElseThrow(NotFoundException.supplier("Genre with id %d not foud", id));
-    }
+    GenreDto findById(long id);
 }
