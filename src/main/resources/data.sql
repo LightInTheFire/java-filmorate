10b88af2a746930b3c127a7a2566b147f396f1e1
Ilia Egorov <aleilmessia@mail.ru>
2025-10-07 08:46:51 +0300

    fix: updated data.sql to use one merge

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,14 @@
-MERGE INTO mpa_ratings (name) KEY (name) VALUES ('G');
-MERGE INTO mpa_ratings (name) KEY (name) VALUES ('PG');
-MERGE INTO mpa_ratings (name) KEY (name) VALUES ('PG-13');
-MERGE INTO mpa_ratings (name) KEY (name) VALUES ('R');
-MERGE INTO mpa_ratings (name) KEY (name) VALUES ('NC-17');
+MERGE INTO mpa_ratings (name) KEY (name)
+VALUES ('G'),
+       ('PG'),
+       ('PG-13'),
+       ('R'),
+       ('NC-17');
 
-MERGE INTO genres (name) KEY (name) VALUES ('Комедия');
-MERGE INTO genres (name) KEY (name) VALUES ('Драма');
-MERGE INTO genres (name) KEY (name) VALUES ('Мультфильм');
-MERGE INTO genres (name) KEY (name) VALUES ('Триллер');
-MERGE INTO genres (name) KEY (name) VALUES ('Документальный');
-MERGE INTO genres (name) KEY (name) VALUES ('Боевик');
+MERGE INTO genres (name) KEY (name)
+VALUES ('Комедия'),
+       ('Драма'),
+       ('Мультфильм'),
+       ('Триллер'),
+       ('Документальный'),
+       ('Боевик');
