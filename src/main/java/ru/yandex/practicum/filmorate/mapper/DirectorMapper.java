38c61de0e8f2e8bd2baecba05b69dd18d05e7cc2
Ilia Egorov <aleilmessia@mail.ru>
2025-10-12 21:33:54 +0300

    feat: added director model, it's dto's, mapper and added tables in schema

---
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/DirectorMapper.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.mapper;
+
+import lombok.experimental.UtilityClass;
+import ru.yandex.practicum.filmorate.dto.director.DirectorDto;
+import ru.yandex.practicum.filmorate.dto.director.NewDirectorRequest;
+import ru.yandex.practicum.filmorate.dto.director.UpdateDirectorRequest;
+import ru.yandex.practicum.filmorate.model.Director;
+
+@UtilityClass
+public class DirectorMapper {
+
+    public DirectorDto toDirectorDto(Director director) {
+        return new DirectorDto(director.getId(), director.getName());
+    }
+
+    public Director toDirector(NewDirectorRequest directorDto) {
+        return Director.builder()
+                .name(directorDto.getName())
+                .build();
+    }
+
+    public Director updateDirectorFields(Director director, UpdateDirectorRequest request) {
+        if (request.hasName()) {
+            director.setName(request.getName());
+        }
+
+        return director;
+    }
+}
