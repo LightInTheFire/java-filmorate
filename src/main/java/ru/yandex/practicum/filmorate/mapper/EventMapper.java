70162f060bd78527dcdf3e08b2c8bf175527ecc1
n20va <99407852+n20va@users.noreply.github.com>
2025-10-17 23:22:48 +0300

    Update EventMapper.java

--- src/main/java/ru/yandex/practicum/filmorate/mapper/EventMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/EventMapper.java
@@ -6,26 +6,15 @@ import ru.yandex.practicum.filmorate.model.Event;
 
 @UtilityClass
 public class EventMapper {
-
+    
     public EventDto toEventDto(Event event) {
         return new EventDto(
-                event.getTimestamp(),
-                event.getUserId(),
-                event.getEventType(),
-                event.getOperation(),
-                event.getEventId(),
-                event.getEntityId()
+            event.getTimestamp(),
+            event.getUserId(),
+            event.getEventType(),
+            event.getOperation(),
+            event.getEventId(),
+            event.getEntityId()
         );
-    }
-
-    public Event toEvent(EventDto eventDto) {
-        return Event.builder()
-                .eventId(eventDto.eventId())
-                .userId(eventDto.userId())
-                .eventType(eventDto.eventType())
-                .operation(eventDto.operation())
-                .entityId(eventDto.entityId())
-                .timestamp(eventDto.timestamp())
-                .build();
     }
 }
