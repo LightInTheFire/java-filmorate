95b24f497427826780885c4f07554ee0665cbb37
n20va <99407852+n20va@users.noreply.github.com>
2025-10-17 21:33:18 +0300

    Update JdbcFriendshipsRepository.java

--- src/main/java/ru/yandex/practicum/filmorate/repository/friendship/JdbcFriendshipsRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/friendship/JdbcFriendshipsRepository.java
@@ -1,10 +1,3 @@
-package ru.yandex.practicum.filmorate.repository.friendship;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-import org.springframework.stereotype.Repository;
-
 @Repository
 @RequiredArgsConstructor
 public class JdbcFriendshipsRepository implements FriendshipsRepository {
@@ -15,7 +8,7 @@ public class JdbcFriendshipsRepository implements FriendshipsRepository {
         MapSqlParameterSource params = getParameterMap(userId, friendId);
         String addFriendshipSql = """
                 INSERT INTO friendships (user_id1, user_id2)
-                VALUES (:user_id1, :user_id2)""";
+                VALUES (:user_id1, :user_id2), (:user_id2, :user_id1)""";
         jdbc.update(addFriendshipSql, params);
     }
 
@@ -24,7 +17,8 @@ public class JdbcFriendshipsRepository implements FriendshipsRepository {
         MapSqlParameterSource params = getParameterMap(userId, friendId);
         String deleteFriendshipSql = """
                 DELETE FROM friendships
-                WHERE user_id1 = :user_id1 AND user_id2 = :user_id2""";
+                WHERE (user_id1 = :user_id1 AND user_id2 = :user_id2)
+                   OR (user_id1 = :user_id2 AND user_id2 = :user_id1)""";
         jdbc.update(deleteFriendshipSql, params);
     }
 
@@ -34,7 +28,8 @@ public class JdbcFriendshipsRepository implements FriendshipsRepository {
                 SELECT EXISTS (
                     SELECT 1
                     FROM friendships
-                    WHERE user_id1 = :user_id AND user_id2 = :friend_id)""";
+                    WHERE (user_id1 = :user_id AND user_id2 = :friend_id)
+                       OR (user_id1 = :friend_id AND user_id2 = :user_id))""";
         MapSqlParameterSource params = new MapSqlParameterSource()
                 .addValue("user_id", userId)
                 .addValue("friend_id", friendId);
