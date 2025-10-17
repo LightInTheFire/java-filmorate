27fbba9ea1661e6728a35b9b2744dba9f336793e
n20va <99407852+n20va@users.noreply.github.com>
2025-10-17 23:00:58 +0300

    Update EventServiceImpl.java

--- src/main/java/ru/yandex/practicum/filmorate/service/EventServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventServiceImpl.java
@@ -4,11 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dto.event.EventDto;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.EventMapper;
 import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.repository.event.EventRepository;
-import ru.yandex.practicum.filmorate.repository.user.UserRepository;
 
 import java.util.List;
 
@@ -17,15 +15,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
     private final EventRepository eventRepository;
-    private final UserRepository userRepository;
 
     @Override
     public List<EventDto> getUserFeed(long userId) {
-        throwIfUserNotFound(userId);
-
         List<Event> events = eventRepository.findByUserId(userId);
         log.trace("Retrieved {} events for user with id: {}", events.size(), userId);
-
+        
         return events.stream()
                 .map(EventMapper::toEventDto)
                 .toList();
@@ -35,10 +30,5 @@ public class EventServiceImpl implements EventService {
     public void addEvent(Event event) {
         eventRepository.save(event);
         log.debug("Event saved: {}", event);
-    }
-
-    private void throwIfUserNotFound(long userId) {
-        userRepository.findById(userId)
-                .orElseThrow(NotFoundException.supplier("User with id %d not found", userId));
     }
 }
