99cea53150ccb14bce37f27d1b9c165eb58ebdf6
n20va <99407852+n20va@users.noreply.github.com>
2025-10-17 22:15:53 +0300

    Update FriendshipController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FriendshipController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FriendshipController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.FriendshipService;
@@ -16,26 +17,28 @@ public class FriendshipController {
     private final FriendshipService friendshipService;
 
     @PutMapping("/{userId}/friends/{friendId}")
+    @ResponseStatus(HttpStatus.OK)
     public void addFriend(@PathVariable long userId, @PathVariable long friendId) {
-        log.info("Adding friend {} to user {}", friendId, userId);
+        log.info("PUT /users/{}/friends/{} - Adding friend", userId, friendId);
         friendshipService.addFriendship(userId, friendId);
     }
 
     @DeleteMapping("/{userId}/friends/{friendId}")
+    @ResponseStatus(HttpStatus.OK)
     public void removeFriend(@PathVariable long userId, @PathVariable long friendId) {
-        log.info("Removing friend {} from user {}", friendId, userId);
+        log.info("DELETE /users/{}/friends/{} - Removing friend", userId, friendId);
         friendshipService.removeFriendship(userId, friendId);
     }
 
     @GetMapping("/{userId}/friends")
     public List<User> getFriends(@PathVariable long userId) {
-        log.info("Getting friends for user {}", userId);
+        log.info("GET /users/{}/friends - Getting user friends", userId);
         return friendshipService.getFriends(userId);
     }
 
     @GetMapping("/{userId}/friends/common/{otherUserId}")
     public List<User> getCommonFriends(@PathVariable long userId, @PathVariable long otherUserId) {
-        log.info("Getting common friends between user {} and user {}", userId, otherUserId);
+        log.info("GET /users/{}/friends/common/{} - Getting common friends", userId, otherUserId);
         return friendshipService.getCommonFriends(userId, otherUserId);
     }
 }
