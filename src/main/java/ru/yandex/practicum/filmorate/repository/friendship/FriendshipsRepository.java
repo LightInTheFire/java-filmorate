b7552037b5ce7f152b392d6fba9d91e3e1fda3c2
n20va <99407852+n20va@users.noreply.github.com>
2025-10-17 22:00:02 +0300

    Update FriendshipsRepository.java

--- src/main/java/ru/yandex/practicum/filmorate/repository/friendship/FriendshipsRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/friendship/FriendshipsRepository.java
@@ -1,13 +1,9 @@
 package ru.yandex.practicum.filmorate.repository.friendship;
 
-import java.util.List;
-
 public interface FriendshipsRepository {
     void addFriendship(long userId, long friendId);
-
+    
     void removeFriendship(long userId, long friendId);
-
+    
     boolean isFriends(long userId, long friendId);
-
-    List<Long> findFriendIdsByUserId(long userId);
 }
