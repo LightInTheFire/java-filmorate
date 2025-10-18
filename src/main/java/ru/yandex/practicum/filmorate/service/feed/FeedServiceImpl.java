1ee81dd6d33a4980b8e875c13a1e44580c247c02
n20va <99407852+n20va@users.noreply.github.com>
2025-10-18 20:39:09 +0300

    Update FeedServiceImpl.java

--- src/main/java/ru/yandex/practicum/filmorate/service/feed/FeedServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/feed/FeedServiceImpl.java
@@ -3,8 +3,12 @@ package ru.yandex.practicum.filmorate.service.feed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dto.event.EventDto;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.mapper.EventMapper;
 import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.model.EventType;
+import ru.yandex.practicum.filmorate.model.Operation;
 import ru.yandex.practicum.filmorate.repository.feed.FeedRepository;
 import ru.yandex.practicum.filmorate.repository.user.UserRepository;
 
@@ -18,19 +22,30 @@ public class FeedServiceImpl implements FeedService {
     private final UserRepository userRepository;
 
     @Override
-    public List<Event> getUserFeed(long userId) {
+    public List<EventDto> getUserFeed(long userId) {
         userRepository.findById(userId)
                 .orElseThrow(NotFoundException.supplier("User with id %d not found", userId));
 
         List<Event> events = feedRepository.findByUserId(userId);
         log.info("Retrieved {} events for user {}", events.size(), userId);
-        return events;
+        
+        return events.stream()
+                .map(EventMapper::toEventDto)
+                .toList();
     }
 
     @Override
-    public void addEvent(Event event) {
+    public void addEvent(EventType eventType, Operation operation, long userId, long entityId) {
+        Event event = Event.builder()
+                .timestamp(System.currentTimeMillis())
+                .userId(userId)
+                .eventType(eventType)
+                .operation(operation)
+                .entityId(entityId)
+                .build();
+        
         feedRepository.save(event);
-        log.debug("Event saved: type={}, operation={}, userId={}, entityId={}",
-                event.getEventType(), event.getOperation(), event.getUserId(), event.getEntityId());
+        log.debug("Event saved: type={}, operation={}, userId={}, entityId={}", 
+            eventType, operation, userId, entityId);
     }
 }
