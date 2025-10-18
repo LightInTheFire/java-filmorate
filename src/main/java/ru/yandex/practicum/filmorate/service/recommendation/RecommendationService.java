ddee8814f299b9d89c0e3f3cf1f5fb3a2764f7bb
n20va <99407852+n20va@users.noreply.github.com>
2025-10-18 18:27:15 +0300

    Delete src directory

--- src/main/java/ru/yandex/practicum/filmorate/service/recommendation/RecommendationService.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.service.recommendation;
-
-import ru.yandex.practicum.filmorate.dto.film.FilmDto;
-
-import java.util.Collection;
-
-public interface RecommendationService {
-    Collection<FilmDto> findFilmRecommendations(long userId);
-}
